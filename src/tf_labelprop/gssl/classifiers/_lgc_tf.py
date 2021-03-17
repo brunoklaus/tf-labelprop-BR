Moved LGC code to TF 2.0; fixed error on fast GTAM computation

src/tf_labelprop/gssl/classifiers/_lgc_tf.py
@@ -8,7 +8,7 @@ import time
 from scipy import sparse
 
 import numpy as np
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 from tf_labelprop.gssl.classifiers import GSSLClassifier
 import tf_labelprop.logging.logger as LOG 
 
@@ -16,8 +16,8 @@ import tf_labelprop.logging.logger as LOG
 def get_S_fromtensor(W):
     wsum = tf.sparse.reduce_sum(W,axis=1)
     wsum = tf.reshape(wsum,(-1,))
-    d_sqrt = tf.reciprocal(tf.sqrt(wsum))
-    d_sqrt = tf.where(tf.is_finite(d_sqrt),d_sqrt,tf.ones(shape=tf.shape(d_sqrt)))
+    d_sqrt = tf.math.reciprocal(tf.sqrt(wsum))
+    d_sqrt = tf.where(tf.math.is_finite(d_sqrt),d_sqrt,tf.ones(shape=tf.shape(d_sqrt)))
     
     d_sqrt_i = tf.gather(d_sqrt,W.indices[:,0])
     d_sqrt_j = tf.gather(d_sqrt,W.indices[:,1])
@@ -62,7 +62,7 @@ def row_normalize(x):
         s= tf.cast(tf.shape(x)[1],tf.float32)
         vec = tf.reduce_sum(x,axis=1)
         x = x/repeat(vec,s)
-        x = tf.where(tf.is_finite(x),x,tf.ones(shape=tf.shape(x))/s)
+        x = tf.where(tf.math.is_finite(x),x,tf.ones(shape=tf.shape(x))/s)
         return x
 """ UTIL FUNTIONS END """
 
@@ -75,62 +75,45 @@ def convert_sparse_matrix_to_sparse_tensor(X,var_values=False):
     
 
 def LGC_iter_TF(X,W,Y,labeledIndexes, alpha = 0.1,num_iter = 1000, hook=None):
-    config = tf.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = 0.5
-    tf.reset_default_graph()
-    with tf.Session(config=config) as sess:
-        
-        """ Set W to sparse if necessary, make copy of Y """
-        W = sparse.csr_matrix(W)        
-        Y = np.copy(Y)
-        
-        """ Convert W to tensor """
-        W = convert_sparse_matrix_to_sparse_tensor(W)
-        LOG.debug(W,LOG.ll.CLASSIFIER)
-        
-        """ Get degree Matrix """
-        D =  tf.sparse.reduce_sum(W,axis=1)
-        
-        
-        """ F_0 is a copy of the label matrix, but we erase the information on labeled Indexes """
-        F_0 = np.copy(Y).astype(np.float32) 
-        F_0[np.logical_not(labeledIndexes),:] = 0.0
-        
-        
-        
-        """
-            CREATE S - Needed for LGC propagation
-        """
-        S =  get_S_fromtensor(W)
-        
-        
-        """
-        CREATE F variable
-        """
-        F = tf.Variable(np.copy(F_0).astype(np.float32),name="F")
-        F_0 = tf.Variable(F_0)
-        TOTAL_ITER = tf.constant(int(num_iter))
-        
-        def _to_dense(sparse_tensor):
-            return tf.sparse_add(tf.zeros(sparse_tensor._dense_shape), sparse_tensor)
-        calc_F = update_F(TOTAL_ITER, alpha, S, F_0)
-        assign_to_F = tf.assign(F,calc_F[1])
-        
-        """
-            Run variable initializers 
-        """
-        global_var_init=tf.global_variables_initializer()
-        local_var_init = tf.local_variables_initializer()
-        sess.run([global_var_init,local_var_init])    
-        
-        c = time.time()
-        sess.run(assign_to_F)
-        elapsed = time.time() - c
-        LOG.info('Label Prop (excluding initialization) done in {:.2} seconds'.format(elapsed),
-                 LOG.ll.CLASSIFIER)
-        
-        result  = F.eval(sess) 
-        sess.close()
-        return result
+    c = time.time()
+    
+    """ Set W to sparse if necessary, make copy of Y """
+    W = sparse.csr_matrix(W)        
+    Y = np.copy(Y)
+    
+    """ Convert W to tensor """
+    W = convert_sparse_matrix_to_sparse_tensor(W)
+    LOG.debug(W,LOG.ll.CLASSIFIER)
+    
+    """ Get degree Matrix """
+    D =  tf.sparse.reduce_sum(W,axis=1)
+    
+    
+    """ F_0 is a copy of the label matrix, but we erase the information on labeled Indexes """
+    F_0 = np.copy(Y).astype(np.float32) 
+    F_0[np.logical_not(labeledIndexes),:] = 0.0
+    
+    
+    
+    """
+        CREATE S - Needed for LGC propagation
+    """
+    S =  get_S_fromtensor(W)
+    
+    
+    """
+    CREATE F variable
+    """
+    F = tf.Variable(np.copy(F_0).astype(np.float32),name="F")
+    F_0 = tf.Variable(F_0)
+    TOTAL_ITER = tf.constant(int(num_iter))
+    for _ in range(num_iter):
+        F = (1-alpha)*F_0 + alpha*tf.sparse.sparse_dense_matmul(S,F)
+    
+    elapsed = time.time() - c
+    LOG.info('Label Prop done in {:.2} seconds'.format(elapsed),
+             LOG.ll.CLASSIFIER)
+    
+    return F.numpy()
 
 

src/tf_labelprop/gssl/classifiers/gtam.py
@@ -60,13 +60,12 @@ class GTAMClassifier(GSSLClassifier):
         #Get graph laplacian
         L = gutils.lap_matrix(W, which_lap='sym')
         #Propagation matrix
-        P = np.linalg.inv( I- 1/(1+mu) *(I-L) )*mu/(1+mu)
+        from scipy.linalg import inv as invert
+        P = invert( I- 1/(1+mu) *(I-L) )*mu/(1+mu)
         
         P_t = P.transpose()
         #Matrix A
         A = ((P_t @ L) @ P) + mu* ((P_t - I) @ (P - I))
-        
-
         #A = A + A.transpose()
         
         W = scipy.sparse.coo_matrix(W)
@@ -91,8 +90,10 @@ class GTAMClassifier(GSSLClassifier):
                 Then, we normalize each row so that row sums to its estimated influence
             '''
             ul = np.logical_not(labeledIndexes)
+            
+            Z = gutils.calc_Z(Y, labeledIndexes, D, estimatedFreq,weigh_by_degree=True)
 
-            Z = gutils.calc_Z(Y, labeledIndexes, D, estimatedFreq,weigh_by_degree=self.weigh_by_degree)
+
             if Q is None:
                 #Compute graph gradient
                 Q = np.matmul(A,Z)
@@ -103,9 +104,10 @@ class GTAMClassifier(GSSLClassifier):
                 
             else:
                 Q[id_min_line,:] = np.inf
-                new_el_pct = Z[id_min_line,id_min_col] / np.sum(Z[:,id_min_col])
+                d_sj = np.sum(Z[labeledIndexes,id_min_col])
+                d_sj1 = d_sj + Z[id_min_line,id_min_col]
                 Q[ul,id_min_col] =\
-                 (1 - new_el_pct) * Q[ul,id_min_col] + Z[id_min_line,id_min_col] * A[ul,id_min_line]
+                 (d_sj/(d_sj1) * Q[ul,id_min_col]) + (Z[id_min_line,id_min_col]/d_sj1 * A[ul,id_min_line])
             
             #Find minimum unlabeled index
             

src/tf_labelprop/gssl/graph/gssl_utils.py
@@ -252,9 +252,8 @@ def calc_Z(Y, labeledIndexes,D,estimatedFreq=None,weigh_by_degree=False):
         Y = init_matrix(Y,labeledIndexes)
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
     assert D.shape[0] == Y.shape[0]
-    Z = np.array(Y)
+    Z = np.zeros_like(Y)
     for i in np.where(labeledIndexes == True)[0]:
-        Z[i,:] = 0
         if weigh_by_degree:
             Z[i,np.argmax(Y[i,:])] = D[i]
         else: